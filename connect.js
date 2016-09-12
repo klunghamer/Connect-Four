1ce791248333685f43464922df7411dbabd46353
Kelly Lunghamer <klung@umich.edu>
2016-09-12 17:12:38 -0400

    Started change color function

---
+++ connect.js
@@ -0,0 +1,39 @@
+//
+var App = {
+  countClicks: 0,
+  // rowRed: 0,
+  // columnRed: 0,
+  // rowBlack: 0,
+  // columnBlack: 0,
+  red: [],
+  black: [],
+  isClicked: function(obj) {
+    if (/*clicks is odd*/) {
+      // create clicked property to be equal to true
+      // add class of red
+      // push new object into red
+    }
+    if (/*clicks is even*/) {
+      //create click property to be true
+      // add class of black
+      //push new object into black
+    }
+  },
+  changeColor: function(obj) {
+    if (obj.color = 'red') {
+      $(this).css('color', 'red');
+    }
+    if (obj.color = 'black') {
+      $(this).css('color', 'black');
+    }
+  }
+}
+
+
+
+
+
+  window.onload = function () {
+    //
+
+  }
