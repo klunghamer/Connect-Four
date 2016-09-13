c65d0757d24a9052bb4cb98fa7252daaab698915
Kelly Lunghamer <klung@umich.edu>
2016-09-12 22:03:46 -0400

    Mad arrays of columns

--- connect.js
+++ connect.js
@@ -1,24 +1,43 @@
-//
-// var App = {
-//   countClicks: 0,
-//   red: [],
-//   black: [],
-//   isClicked: function(childOfSection) {
-    // if (clicks is odd) {
-      // create clicked property to be equal to true
-      // add class of red
-      // push new object into red
-    // }
-    // if (clicks is even) {
-      //create click property to be true
-      // add class of black
-      //push new object into black
-//     }
-//   },
-//   connectFour: function (red or black arrays){
-//
-//   }
-// }
+
+var App = {
+  countClicks: 0,
+  column1: [],
+  column2: [],
+  column3: [],
+  column4: [],
+  column5: [],
+  column6: [],
+  column7: [],
+  createArrays: function () {
+    App.column1.push($('.column1').children());
+    App.column2.push($('.column2').children());
+    App.column3.push($('.column3').children());
+    App.column4.push($('.column4').children());
+    App.column5.push($('.column5').children());
+    App.column6.push($('.column6').children());
+    App.column7.push($('.column7').children());
+    // console.log(App.column7);
+    console.log(App.countClicks);
+  }
+  // countClicks: 0,
+  // red: [],
+  // black: [],
+  // isClicked: function(childOfSection) {
+  //   if (clicks is odd) {
+  //     create clicked property to be equal to true
+  //     add class of red
+  //     push new object into red
+  //   }
+  //   if (clicks is even) {
+  //     create click property to be true
+  //     add class of black
+  //     push new object into black
+  //   }
+  // },
+  // connectFour: function (red or black arrays){
+  //
+  // }
+}
 
 var UI = {
   // changeColor: function(obj) {
@@ -29,10 +48,12 @@ var UI = {
   //       $(this).css('color', 'black');
   //     }
   // },
-
-  clickBoard: function() {
-    console.log('hi');
-  }
+  // clickBoard: function(column) {
+  //   console.log('hi');
+  //
+  //   console.log($(this).children().length - UI.countClicks);
+  //   UI.countClicks++;
+  // }
 }
 
 
@@ -40,5 +61,6 @@ var UI = {
   window.onload = function () {
     //select last child of what is clicked
     //update it to be the 1 - minus the last child
-    $('section').on('click', UI.clickBoard);
+    $('.column1').on('click', App.createArrays);
+    $('.column2').on('click', UI.clickBoard);
   };
