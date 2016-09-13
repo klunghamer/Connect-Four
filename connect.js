4cebfea8a4a952921c565b008ab984e4dc35e993
Kelly Lunghamer <klung@umich.edu>
2016-09-12 20:41:53 -0400

    Click event on column works

--- connect.js
+++ connect.js
@@ -1,39 +1,44 @@
 //
-var App = {
-  countClicks: 0,
-  // rowRed: 0,
-  // columnRed: 0,
-  // rowBlack: 0,
-  // columnBlack: 0,
-  red: [],
-  black: [],
-  isClicked: function(obj) {
-    if (/*clicks is odd*/) {
+// var App = {
+//   countClicks: 0,
+//   red: [],
+//   black: [],
+//   isClicked: function(childOfSection) {
+    // if (clicks is odd) {
       // create clicked property to be equal to true
       // add class of red
       // push new object into red
-    }
-    if (/*clicks is even*/) {
+    // }
+    // if (clicks is even) {
       //create click property to be true
       // add class of black
       //push new object into black
-    }
-  },
-  changeColor: function(obj) {
-    if (obj.color = 'red') {
-      $(this).css('color', 'red');
-    }
-    if (obj.color = 'black') {
-      $(this).css('color', 'black');
-    }
+//     }
+//   },
+//   connectFour: function (red or black arrays){
+//
+//   }
+// }
+
+var UI = {
+  // changeColor: function(obj) {
+  //     if (obj.color = 'red') {
+  //       $(this).css('color', 'red');
+  //     }
+  //     if (obj.color = 'black') {
+  //       $(this).css('color', 'black');
+  //     }
+  // },
+
+  clickBoard: function() {
+    console.log('hi');
   }
 }
 
 
 
-
-
   window.onload = function () {
-    //
-
-  }
+    //select last child of what is clicked
+    //update it to be the 1 - minus the last child
+    $('section').on('click', UI.clickBoard);
+  };
