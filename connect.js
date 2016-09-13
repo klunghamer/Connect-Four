94f05783774285835b650f89b212589da3f7f232
Kelly Lunghamer <klung@umich.edu>
2016-09-13 13:00:24 -0400

    Reorganized app logic

--- connect.js
+++ connect.js
@@ -1,34 +1,41 @@
 
-var App = {
-  countClicks: 0,
-  column1: [],
-  column2: [],
-  column3: [],
-  column4: [],
-  column5: [],
-  column6: [],
-  column7: [],
-  createArrays: function () {
-    App.column1.push($('.column1').children());
-    App.column2.push($('.column2').children());
-    App.column3.push($('.column3').children());
-    App.column4.push($('.column4').children());
-    App.column5.push($('.column5').children());
-    App.column6.push($('.column6').children());
-    App.column7.push($('.column7').children());
-    // console.log(App.column7);
-    // App.countClicks++;
-    console.log(App.countClicks);
-  },
 
-  assignColor: function () {
-    App.countClicks++;
-    if(App.countClicks%2 === 0) {
-      console.log('red');
-    } else {
-      console.log('black');
-    }
+var App = {
+  player: true,
+  createArrays: function () {
+    column1 = $('.column1');
+    column2 = $('.column2');
+    column3 = $('.column3');
+    column4 = $('.column4');
+    column5 = $('.column5');
+    column6 = $('.column6');
+    column6 = $('.column7');
+
+    // column2 = $('.column2').children().toArray();
+    // column3 = $('.column3').children().toArray();
+    // column4 = $('.column4').children().toArray();
+    // column5 = $('.column5').children().toArray();
+    // column6 = $('.column6').children().toArray();
+    // column7 = $('.column7').children().toArray();
+    // console.log($('.column1:nth-child(1)').addClass('red'));
+    // console.log(column1);
+},
+
+  assignColor: function (arrays) {
+    console.log(column1.children().eq(0).addClass('red'));
+    // if(App.player) {
+    //   column1[6].addClass('true');
+    //   console.log(column1[6]);
+      // App.player = !App.player;
+
+
+    // }
+      //  else {
+      //   console.log('black');
+      // }
+
   }
+}
 
   // countClicks: 0,
   // red: [],
@@ -48,9 +55,9 @@ var App = {
   // connectFour: function (red or black arrays){
   //
   // }
-}
+// }
 
-var UI = {
+// var UI = {
   // changeColor: function(obj) {
   //     if (obj.color = 'red') {
   //       $(this).css('color', 'red');
@@ -65,13 +72,14 @@ var UI = {
   //   console.log($(this).children().length - UI.countClicks);
   //   UI.countClicks++;
   // }
-}
+// }
 
 
 
   window.onload = function () {
     //select last child of what is clicked
     //update it to be the 1 - minus the last child
+    App.createArrays();
     $('.column1').on('click', App.assignColor);
     $('.column2').on('click', App.assignColor);
   };
