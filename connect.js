764ff7eebf7a0cc29d9e41713c1af0ebeec98d21
Kelly Lunghamer <klung@umich.edu>
2016-09-15 16:00:27 -0400

    Working on CSS

--- connect.js
+++ connect.js
@@ -11,6 +11,7 @@ var App = {
     App.addColorProp(token, columnNum);
     App.findFourVertical(columnNum);
     App.findFourHorizontal(columnNum);
+    UI.playerTurn();
   },
   addColorProp: function (token, columnNum) {
     if (App.player) {
@@ -22,7 +23,7 @@ var App = {
       App.board[columnNum].unshift(token);
       App.player = !App.player;
     }
-      console.log(App.board);
+      // console.log(App.board);
       // UI.changeColors(token, columnNum);
   },
 
@@ -33,6 +34,7 @@ var App = {
     for (var i = 0; i < column.length; i++) {
       if(column[i].color === 'red') {
        winningColumnRed.push(i);
+      //  console.log(winningColumnRed);
      } else if (column[i].color === 'black'){
        winningColumnBlack.push(i);
      }
@@ -58,10 +60,10 @@ var App = {
         // console.log(App.board[i][j]);
         if(App.board[i][j].color === 'red') {
          winningRowRed.push(j);
-         console.log(winningRowRed);
+        //  console.log(winningRowRed);
        } else if (App.board[i][j].color === 'black'){
          winningRowBlack.push(j);
-         console.log(winningRowBlack);
+        //  console.log(winningRowBlack);
        }
      }
     }
@@ -80,18 +82,29 @@ var App = {
 
 var UI = {
   changeColors: function () {
-
-
     if (App.player === false) {
       $(this).css('backgroundColor', 'rgb(255,0,0)');
     } else {
       $(this).css('backgroundColor', 'rgb(0,0,0)');
     }
+  },
+  playerTurn: function () {
+    if (App.player === false) {
+      $('#player1').hide();
+      $('#player2').show();
+    }
+    if (App.player === true) {
+      $('#player2').hide();
+      $('#player1').show();
+    }
+    console.log(App.player);
   }
 }
 
 
 window.onload = function () {
+  $('#player2').hide();
   $('div').one('click', App.getColumn);
   $('div').one('click', UI.changeColors);
+  $('div').on('click', UI.playerTurn);
 };
